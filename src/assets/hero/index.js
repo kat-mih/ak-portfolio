68960c9d55da0ed28031cb9a9e559f2e769ca701
Kate S <kateshpudejko@gmail.com>
2023-08-20 16:53:41 +0200

    hero right section: swiper added

---
+++ src/assets/hero/index.js
@@ -0,0 +1,6 @@
+import one from "./main_right.jpeg";
+import two from "./main_right_third.jpeg";
+import three from "./main_right_second.jpeg";
+import four from "./main_right_forth.jpeg";
+
+export { one, two, three, four };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import "./Hero.css";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+import { Autoplay, EffectFade } from "swiper/modules";
 
 import left from "../assets/main_left.jpeg";
-import right from "../assets/main_right.jpeg";
+import { one, two, three, four } from "../assets/hero";
 
 const Hero = () => {
+  const heroRightImages = [one, two, three, four];
+
   return (
     <div className="hero">
       <div className="hero__left">
@@ -13,7 +21,22 @@ const Hero = () => {
         <p>б'юті-експерт|естетика|PMU</p>
       </div>
       <div className="hero__right">
-        <img src={right} alt="" />
+        {/* <img src={right} alt="" /> */}
+        <Swiper
+          effect={"fade"}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Autoplay]}
+          className="hero__swiper">
+          {heroRightImages.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
         <p className="hero__right-header">Aнна Kапiтaнюк</p>
       </div>
     </div>
