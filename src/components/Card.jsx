1b1b851d6ae90583760ba8a76f7455a296d49617
Kate S <kateshpudejko@gmail.com>
2023-10-11 11:51:21 +0200

    about and courses sections changed(content)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,15 @@ import React, { useState } from "react";
 import CardMore from "./CardMore";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ name, description }) => {
   const [openMore, setOpenMore] = useState(false);
 
   return (
     <>
       <div className="card">
-        <header className="card__header">Пудрові брови</header>
+        <header className="card__header">{name}</header>
         <div className="card__body">
-          Курс в форматі онлайн уроків 8 основних уроків у записі + додаткові
-          матеріали (гайди, чек-лист, графічні зображення) <br />
-          <span>
-            Додатковий урок - "Якісна зйомка робіт на телефон та обробка фото"
-          </span>
+          {description}
           <CardMore open={openMore} />
           <button
             className="card__body-btn"
--- src/sections/Courses.jsx
+++ src/sections/Courses.jsx
@@ -3,13 +3,37 @@ import Card from "../components/Card";
 import "./Courses.css";
 
 const Courses = () => {
+  const coursesInfo = [
+    {
+      name: "Пудрові брови та Акварельні губи. База",
+      description:
+        "Розпочни зі мною свій шлях у світі перманентного макіяжу - ти навчишся актуальним технікам створення перманентних брів і губ та отримаєш прибуткову професію в сфері б'юті",
+    },
+    {
+      name: "Пудрові брови, Акварельні губи та Міжвійка. База",
+      description:
+        "Опануєш сучасну авторську методику створення одразу трьох зон - брови, губи та міжвійка. Поринь у навчання на поглибленому базовому курсі  - стань універсальним майстром перманентного макіяжу",
+    },
+    {
+      name: "Курс підвищення кваліфікації",
+      description:
+        "Курс призначений для майстрів, які хочуть поліпшити свої навички, вдосконалити техніку роботи.",
+    },
+  ];
+
   return (
     <div id="courses" className="courses">
       <h2 className="courses__header">
         Hавчайся <span>у</span> мене
       </h2>
       <div className="courses__cards">
-        <Card />
+        {coursesInfo.map((course) => (
+          <Card
+            key={course.name}
+            name={course.name}
+            description={course.description}
+          />
+        ))}
       </div>
     </div>
   );
